7f8ccda9fc11fae78026907c5c0942763e9612d0
Antonio Soliman <soliman.antonio@iisgalvanimi.edu.it>
2025-01-21 10:38:06 +0000

    continuazione parte 2

--- app.py
+++ app.py
@@ -2,15 +2,13 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from flask_bcrypt import Bcrypt
-import requests
 from models import db, User
 
-app = Flask (__name__)
+app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///utenti.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'your_secret_key'
-
 
 db.init_app(app)
 bcrypt = Bcrypt(app)
@@ -18,10 +16,9 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-
+# Creazione delle tabelle al primo avvio
 with app.app_context():
     db.create_all()
-
 
 @login_manager.user_loader
 def load_user(id_user):
@@ -30,7 +27,6 @@ def load_user(id_user):
 @app.route('/home')
 @login_required
 def home():
-   
     return render_template('home.html', username=current_user.username)
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -47,12 +43,14 @@ def register():
         try:
             db.session.add(new_user)
             db.session.commit()
+
+            # Login automatico dell'utente appena registrato
+            login_user(new_user)
             return redirect(url_for('home'))
         except Exception as e:
             db.session.rollback()
             return render_template('registrazione.html', error=f"Errore durante la registrazione: {e}")
     return render_template('registrazione.html', error=None)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
